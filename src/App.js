ce458f0cb9d87cc6307b3427022fbee28361b5bf
vincent <vincent.arteaga@gmail.com>
2021-07-08 18:35:21 -0700

    assignment complete

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
+  render() {
+    return <div>this is App</div>;
+  }
+}
+
+export default App;
